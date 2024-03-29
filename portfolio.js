c1b017ffd286b01c2dcdfa2d8443e8e2dfb6a528
NickParamore <112004146+NickParamore@users.noreply.github.com>
2024-03-28 23:46:32 -0400

    nav and gallery

--- portfolio.js
+++ portfolio.js
@@ -16,3 +16,48 @@ const navSlide = () => {
     });
 }
 navSlide();
+
+window.addEventListener('load', function() {
+    adjustNavLinksPosition();
+});
+
+window.addEventListener('resize', function() {
+    adjustNavLinksPosition();
+});
+
+function adjustNavLinksPosition() {
+    var navHeight = document.querySelector('nav').offsetHeight;
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.style.top = navHeight + 'px';
+}
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the image and insert it inside the modal
+var images = document.querySelectorAll(".gallery-image");
+var modalImg = document.getElementById("modal-image");
+var captionText = document.getElementById("caption");
+
+images.forEach(function(img) {
+    img.addEventListener('click', function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    });
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close the modal when clicking outside of it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
