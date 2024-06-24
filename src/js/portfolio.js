1abf77799aca2b21cf04673382338cfa2ea2c657
NickParamore <112004146+NickParamore@users.noreply.github.com>
2024-06-24 15:50:53 -0400

    nav changes

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -4,18 +4,3 @@ hamburger.onclick = function (){
     navBar = document.querySelector(".nav-links");
     navBar.classList.toggle("active");
 }
-
-//function that keep the mobile nav-links under the actual nav bar whenever resizing or loading
-window.addEventListener('load', function() {
-    adjustNavLinksPosition();
-});
-
-window.addEventListener('resize', function() {
-    adjustNavLinksPosition();
-});
-
-function adjustNavLinksPosition() {
-    var navHeight = document.querySelector('nav').offsetHeight;
-    var navLinks = document.querySelector('.nav-links');
-    navLinks.style.top = navHeight + 'px';
-}
